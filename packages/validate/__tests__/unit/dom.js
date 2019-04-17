f2637a9ef25e419b6e54014b528248d277bdea0d
Mick Perkins <mickperkins@gmail.com>
2019-04-17 22:33:05 +0100

    Adds validation unit tests

--- packages/validate/__tests__/unit/dom.js
+++ packages/validate/__tests__/unit/dom.js
@@ -1,6 +1,8 @@
 import { 
     h,
-    createErrorTextNode
+    createErrorTextNode,
+    focusFirstInvalidField,
+    createButtonValueNode
 } from '../../src/lib/dom';
 import { DOTNET_CLASSNAMES } from '../../src/lib/constants';
 
@@ -37,24 +39,65 @@ describe('Validate > Unit > DOM > createErrorTextNode', () => {
     });
 });
 
+
+/**
+ * To do: rationalise error rendering and clean-up
+ */
+
 //clearError
-describe('Validate > Unit > DOM > clearError', () => {
-});
-
-//clearErrors
-describe('Validate > Unit > DOM > clearErrors', () => {
-});
-
+// describe('Validate > Unit > DOM > clearError', () => {
+// });
+//clearError
+// describe('Validate > Unit > DOM > clearErrors', () => {
+// });
 //renderErrors
-describe('Validate > Unit > DOM > renderErrors', () => {
-});
-
+// describe('Validate > Unit > DOM > renderErrors', () => {
+// });
 //renderError
-describe('Validate > Unit > DOM > renderError', () => {
-});
+// describe('Validate > Unit > DOM > renderError', () => {
+// });
 
 //focusFirstInvalidField
-describe('Validate > Unit > DOM > focusFirstInvalidField', () => {});
+describe('Validate > Unit > DOM > focusFirstInvalidField', () => {
+    it('should focus on the first invalid field in a form post-vaidation', async () => {
+        document.body.innerHTML = `<form class="form" method="post" action="">
+            <label for="group1">Text (required, min 2 characters, max 8 characters)</label>
+            <input id="group1" name="group1" data-val="true" data-val-length="Please enter between 2 and 8 characters" data-val-required="This field is required" data-val-length-min="2" data-val-length-max="8" type="text">
+            <span class="text-danger field-validation-valid" data-valmsg-for="group1" data-valmsg-replace="true"></span>
+            <label for="group2">Text (required, min 2 characters, max 8 characters)</label>
+            <input id="group2" name="group2" data-val="true" data-val-length="Please enter between 2 and 8 characters" data-val-required="This field is required" data-val-length-min="2" data-val-length-max="8" type="text">
+            <span class="text-danger field-validation-valid" data-valmsg-for="group2" data-valmsg-replace="true"></span>
+        </form>`;
+        const field = document.querySelector('#group2');
+        const mockState = {
+            groups: {
+                group1: {
+                    valid: true,
+                    fields: []
+                },
+                group2: {
+                    valid: false,
+                    fields: [field]
+                }
+            }
+        };
+        focusFirstInvalidField(mockState);
+        expect(document.activeElement).toEqual(field);
+    });
+});
 
 //createButtonValueNode
-describe('Validate > Unit > DOM > createButtonValueNode', () => {});
+describe('Validate > Unit > DOM > createButtonValueNode', () => {
+    it('should a hidden field duplicate of a given field, for conferring submit button values', async () => {
+        document.body.innerHTML = `<form class="form" method="post" action="">
+            <button name="continue" value="1">Continue</button>
+        </form>`;
+        const node = document.getElementsByName('continue')[0];
+        const form = document.querySelector('.form');
+        createButtonValueNode(node, form);
+        const lastElement = form.lastElementChild;
+        expect(lastElement.tagName).toEqual('INPUT');
+        expect(lastElement.getAttribute('name')).toEqual('continue');
+        expect(lastElement.getAttribute('value')).toEqual('1');
+    });
+});
--- packages/validate/__tests__/unit/reducers.js
+++ packages/validate/__tests__/unit/reducers.js
@@ -1,12 +1,177 @@
+import { ACTIONS } from '../../src/lib/constants';
+import Reducers from '../../src/lib/reducers';
 
+/*
+SET_INITIAL_STATE: 'SET_INITIAL_STATE',
+    CLEAR_ERRORS: 'CLEAR_ERRORS',
+    VALIDATION_ERRORS: 'VALIDATION_ERRORS',
+    VALIDATION_ERROR: 'VALIDATION_ERROR',
+    CLEAR_ERROR: 'CLEAR_ERROR',
+    ADD_VALIDATION_METHOD: 'ADD_VALIDATION_METHOD'
+*/
 
-const init = () => {
-    document.body.innerHTML = ``;
-};
+//set initial state
+describe('Validate > Unit > Reducers > Set initial state', () => {
+  	it('should compose a new object based on default empty state object and initial payload', async () => {
+        const state = {};
+        const nextState = {
+            form: document.createElement('form'),
+            groups: {
+                group1: {
+                    valid: false,
+                    fields: [document.createElement('input')],
+                    validators: []
+                }
+            },
+            errorNodes: {},
+            realTimeValidation: false
+        };
+        const output = Reducers[ACTIONS.SET_INITIAL_STATE](state, nextState);
+        expect(output).toEqual(nextState);
+    });
+});
 
-beforeAll(init);
+//clear errors
+describe('Validate > Unit > Reducers > Clear errors', () => {
+  	it('should compose a new object with each group Object containing an empty array of errorMessages and a true validity property', async () => {
+        const state = {
+            form: document.createElement('form'),
+            groups: {
+                group1: {
+                    fields: [document.createElement('input')],
+                    validators: [],
+                    errorMessages: ['This field is required'],
+                    valid: false
+                },
+                group2: {
+                    fields: [document.createElement('input')],
+                    validators: [],
+                    errorMessages: ['This field is required'],
+                    valid: false
+                }
+            },
+            errorNodes: {},
+            realTimeValidation: false
+        };
+        const output = Reducers[ACTIONS.CLEAR_ERRORS](state);
+        expect(output).toEqual({
+            form: document.createElement('form'),
+            groups: {
+                group1: {
+                    fields: [document.createElement('input')],
+                    validators: [],
+                    errorMessages: [],
+                    valid: true
+                },
+                group2: {
+                    fields: [document.createElement('input')],
+                    validators: [],
+                    errorMessages: [],
+                    valid: true
+                }
+            },
+            errorNodes: {},
+            realTimeValidation: false
+        });
+    });
+});
 
-describe('Validate > Unit > Reducers', () => {
-  	it('', async () => {
+//clear error
+describe('Validate > Unit > Reducers > Clear error', () => {
+  	it('should compose a new object with a group Object containing an empty array of errorMessages and a true validity property for a given group name', async () => {
+        const state = {
+            form: document.createElement('form'),
+            groups: {
+                group1: {
+                    fields: [document.createElement('input')],
+                    validators: [],
+                    errorMessages: ['This field is required'],
+                    valid: false
+                },
+                group2: {
+                    fields: [document.createElement('input')],
+                    validators: [],
+                    errorMessages: ['This field is required'],
+                    valid: false
+                }
+            },
+            errorNodes: {},
+            realTimeValidation: false
+        };
+        const output = Reducers[ACTIONS.CLEAR_ERROR](state, 'group1');
+        expect(output).toEqual({
+            form: document.createElement('form'),
+            groups: {
+                group1: {
+                    fields: [document.createElement('input')],
+                    validators: [],
+                    errorMessages: [],
+                    valid: true
+                },
+                group2: {
+                    fields: [document.createElement('input')],
+                    validators: [],
+                     errorMessages: ['This field is required'],
+                    valid: false
+                }
+            },
+            errorNodes: {},
+            realTimeValidation: false
+        });
     });
 })
+
+//add validation errors
+// describe('Validate > Unit > Reducers > Add validation errors', () => {
+//   	it('should compose a new object with group Objects containing an array of errorMessages and a false validity property', async () => {
+//         const state = {
+//             groups: {
+//                 group1: {
+//                     fields: [document.createElement('input')],
+//                     validators: [],
+//                     valid: true
+//                 },
+//                 group2: {
+//                     fields: [document.createElement('input')],
+//                     validators: [],
+//                     valid: true
+//                 }
+//             }
+//         };
+//         const nextState = {
+//             groups: {
+//                 group1: {
+//                     fields: [document.createElement('input')],
+//                     validators: [],
+//                     errorMessages: ['This field is required'],
+//                     valid: false
+//                 },
+//                 group2: {
+//                     fields: [document.createElement('input')],
+//                     validators: [],
+//                     errorMessages: ['This field is required'],
+//                     valid: false
+//                 }
+//             }
+//         };
+//         const output = Reducers[ACTIONS.VALIDATION_ERRORS](state, nextState);
+//         expect(output).toEqual({
+//             realTimeValidation: true,
+//             groups: {
+//                 group1: {
+//                     fields: [document.createElement('input')],
+//                     validators: [],
+//                     errorMessages: ['This field is required'],
+//                     valid: false
+//                 },
+//                 group2: {
+//                     fields: [document.createElement('input')],
+//                     validators: [],
+//                     errorMessages: ['This field is required'],
+//                     valid: false
+//                 }
+//             },
+//             errorNodes: {},
+//         });
+//       });
+// });
