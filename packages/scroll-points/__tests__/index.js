37be569ac6783cb2281718959be9d96151f1b3e7
Mick Perkins <mick.perkins@stormid.com>
2019-10-15 10:29:30 +0100

    Adds IntersectionObserver mock

--- packages/scroll-points/__tests__/index.js
+++ packages/scroll-points/__tests__/index.js
@@ -1,54 +1,59 @@
-import Component from '../src';
+window.IntersectionObserver = jest.fn(function() {
+  this.observe = () => {}
+  // this.disconnect = disconnect
+});
+
+import ScrollPoints from '../src';
 
 let basic, withCallback;
 const init = () => {
     // Set up our document body
-    document.body.innerHTML = `<div class="js-boilerplate test"></div>
-             <div class="js-boilerplate test-2"></div>
-             <div class="js-boilerplate-two test-3"></div>`;
+    document.body.innerHTML = `<div class="js-scroll-point test"></div>
+             <div class="js-scroll-point test-2"></div>
+             <div class="js-scroll-point-two test-3"></div>`;
 
-    basic = Component.init('.js-boilerplate');
-    withCallback = Component.init.call(Component, '.js-boilerplate-two', {
+    basic = ScrollPoints.init('.js-scroll-point');
+    withCallback = ScrollPoints.init('.js-scroll-point-two', {
       callback(){
-        this.node.classList.toggle('callback-test');
+        // this.node.classList.toggle('callback-test');
       }
     });
 };
 
 describe(`Initialisation`, () => {
-    
-    beforeAll(init);
 
+    beforeEach(async () => {
+      const mockEntry = { isIntersecting: false }    
+
+      const observerCallback = IntersectionObserver.mock.calls[0][0];
+      observerCallback([mockEntry]);
+    });
+
+    beforeAll(init);
 
     it('should return array of length 2', async () => {
       expect(basic.length).toEqual(2);
     });
 
     it('each array item should be an object with DOMElement, settings, init, and  handleClick properties', () => {
-
         expect(basic[0]).not.toBeNull();
         expect(basic[0].node).not.toBeNull();
         expect(basic[0].settings).not.toBeNull();
         expect(basic[0].init).not.toBeNull();
         expect(basic[0].handleClick).not.toBeNull();
-
     });
 
      it('should initialisation with different settings if different options are passed', () => {
-
-        expect(basic[0].settings.callback).not.toEqual(withCallback[0].settings.callback);
-    
+        expect(basic[0].settings.callback).not.toEqual(withCallback[0].settings.callback);    
     }); 
 
 });
 
 describe('Options', () => {
 
-  it('should be passed in options', () => {
-    
+  it('should be passed in options', () => {    
     expect(withCallback[0].settings.callback).not.toBeNull();
-    expect(basic[0].settings.callback).toBeNull();
-
+    expect(basic[0].settings.callback).toEqual(false);
   });
 
 });
