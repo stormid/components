9645caab63b4503cf031402133693e5f399568a3
Mick Perkins <mickperkins@gmail.com>
2019-10-21 22:03:51 +0100

    Removes measure from master

--- packages/measure/src/lib/auto-ecommerce/auto.js
+++
@@ -1,9 +0,0 @@
-import { impressions, click, detail, add, purchase } from './';
-
-export default __m => {
-    impressions(__m); 
-    click(__m);
-    detail(__m);
-    add(__m);
-    purchase(__m);
-};
