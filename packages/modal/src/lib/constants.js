08fa50a664b418a427f41a3f5b664a04f6dd4e15
Mick Perkins <mick.perkins@stormid.com>
2019-11-11 16:18:04 +0000

    Fix modal return focus to button that opens modal on close

--- packages/modal/src/lib/constants.js
+++ packages/modal/src/lib/constants.js
@@ -1,6 +1,6 @@
 /* istanbul ignore file */
 // DOM events on a trigger that initiate toggle
-export const TRIGGER_EVENTS = ['ontouchstart' in window ? 'touchstart' : 'click', 'keydown' ];
+export const TRIGGER_EVENTS = ['ontouchend' in window ? 'touchend' : 'click', 'keydown' ];
 
 // Event keycodes that initiate toggle for keyboard events
 export const KEYCODES = [32, 13];
