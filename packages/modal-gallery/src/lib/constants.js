0a41a4862c3a5991f2acdae39f5cda1f605cd202
Mick Perkins <mick.perkins@stormid.com>
2020-03-21 20:56:58 +0000

    Issue/94 (#98)
    * Updates babel and jest config formatting
    
    * Updates trigger events to standardise button triggers across cookke banner, omdal, modal-gralery and toggle, re #94

--- packages/modal-gallery/src/lib/constants.js
+++ packages/modal-gallery/src/lib/constants.js
@@ -6,6 +6,6 @@ export const KEY_CODES = {
     RIGHT: 39
 };
 
-export const TRIGGER_EVENTS = ['ontouchstart' in window ? 'touchstart' : 'click', 'keydown' ];
+export const TRIGGER_EVENTS = window.PointerEvent ? ['pointerup', 'keydown'] : ['ontouchend' in window ? 'ontouchend' : 'click', 'keydown' ];
 
 export const FOCUSABLE_ELEMENTS = ['a[href]', 'area[href]', 'input:not([disabled])', 'select:not([disabled])', 'textarea:not([disabled])', 'button:not([disabled])', 'iframe', 'object', 'embed', '[contenteditable]', '[tabindex]:not([tabindex="-1"])'];
