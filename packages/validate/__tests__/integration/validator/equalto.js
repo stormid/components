31bed26c6e7341cf56d56b842862c4db28282a5c
Mick Perkins <mick.perkins@stormid.com>
2020-06-15 13:30:49 +0100

    Updates dependencies; adds equalto validate tests and fixes documentation (#109)

--- packages/validate/__tests__/integration/validator/equalto.js
+++ packages/validate/__tests__/integration/validator/equalto.js
@@ -2,6 +2,58 @@ import { validate, assembleValidationGroup } from '../../../src/lib/validator';
 
 describe('Validate > Integration > validator > equalto', () => {
     
+    it('should return the validityState false for data-val equalto validator with a value not matching another single field', async () => {
+        expect.assertions(1);
+        document.body.innerHTML = `<input
+                class="input"
+                type="email"
+                id="mce-EMAIL"
+                name="EMAIL"
+                data-val="true"
+                data-val-required="Required error message"
+                value="example@stormid.com"
+            />
+            <input
+                class="input"
+                type="email"
+                id="VERIFY-EMAIL"
+                name="VERIFY-EMAIL"
+                value="not.matching@stormid.com"
+                data-val="true"
+                data-val-equalto="'Verify email' must match 'email'"
+                data-val-equalto-other="EMAIL"
+            />`;
+        const input = document.querySelector('#VERIFY-EMAIL');
+        const group = assembleValidationGroup({}, input)['VERIFY-EMAIL'];
+        expect(validate(group, group.validators[0])).toEqual(false);
+    });
+
+    it('should return the validityState true for data-val equalto validator with a value matching another single field', async () => {
+        expect.assertions(1);
+        document.body.innerHTML = `<input
+                class="input"
+                type="email"
+                id="mce-EMAIL"
+                name="EMAIL"
+                data-val="true"
+                data-val-required="Required error message"
+                value="example@stormid.com"
+            />
+            <input
+                class="input"
+                type="email"
+                id="VERIFY-EMAIL"
+                name="VERIFY-EMAIL"
+                value="example@stormid.com"
+                data-val="true"
+                data-val-equalto="'Verify email' must match 'email'"
+                data-val-equalto-other="EMAIL"
+            />`;
+        const input = document.querySelector('#VERIFY-EMAIL');
+        const group = assembleValidationGroup({}, input)['VERIFY-EMAIL'];
+        expect(validate(group, group.validators[0])).toEqual(true);
+    });
+
     it('should return the validityState false for data-val equalto validator with a value not matching the other field(s)', async () => {
         expect.assertions(1);
         document.body.innerHTML = `<input
