5629f3db6358ac52048b2392f2b911b055153ea9
Mick Perkins <mick.perkins@stormid.com>
2020-08-07 11:16:27 +0100

    Fixes toggle trapTab and focus bugs

--- packages/toggle/example/src/js/index.js
+++ packages/toggle/example/src/js/index.js
@@ -1,6 +1,10 @@
 import toggle from '../../../src';
     
 window.addEventListener('DOMContentLoaded', () => {
-    window.__t1__ = toggle('.js-toggle');
+    window.__t1__ = toggle('.js-toggle', {
+        focus: true,
+        closeOnBlur: true,
+        trapTab: true
+    });
     window.__t2__ = toggle('.js-toggle__local', { closeOnBlur: true });
 });
