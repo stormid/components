3ceab449e026a4d32cd8877279284339f4ae303c
Mick Perkins <mick.perkins@stormid.com>
2021-02-24 12:39:14 +0000

    Modal > Remove hidden input from focusable element selector

--- packages/modal/src/lib/constants.js
+++ packages/modal/src/lib/constants.js
@@ -6,4 +6,4 @@ export const TRIGGER_EVENTS = ['click', 'keydown'];
 export const KEYCODES = [32, 13];
 
 //Array of focusable child elements
-export const FOCUSABLE_ELEMENTS = ['a[href]', 'area[href]', 'input:not([disabled])', 'select:not([disabled])', 'textarea:not([disabled])', 'button:not([disabled])', 'iframe', 'object', 'embed', '[contenteditable]', '[tabindex]:not([tabindex="-1"])'];
+export const FOCUSABLE_ELEMENTS = ['a[href]', 'area[href]', 'input:not([disabled]):not([type=hidden])', 'select:not([disabled])', 'textarea:not([disabled])', 'button:not([disabled])', 'iframe', 'object', 'embed', '[contenteditable]', '[tabindex]:not([tabindex="-1"])'];
