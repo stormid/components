87c039087e38cdde09219082a7e35453bbddce09
Mick Perkins <mick.perkins@stormid.com>
2021-04-22 22:14:42 +0100

    Remove measure package

--- packages/measure/src/lib/auto/ecommerce/checkout-step.js
+++
@@ -1,9 +0,0 @@
-import { DATA_ATTRIBUTES } from '../constants';
-import { composeCheckoutAction } from '../shared';
-
-export default __m => {
-    const node = document.querySelector(`[${DATA_ATTRIBUTES.CHECKOUT_STEP}]`);
-    if (!node) return;
-    
-    __m.ecommerce.action(composeCheckoutAction({ node, action: 'checkout', event: 'Checkout' }));
-};
