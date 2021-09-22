f69d57905d26a6e8158322ab7a3394dec52efabd
Mick Perkins <mick.perkins@stormid.com>
2021-09-22 14:09:21 +0100

    Cookie banner missing tid bug fix
    Add warning for missing tid cookie banner init
    
    Add tests

--- packages/cookie-banner/src/lib/factory.js
+++ packages/cookie-banner/src/lib/factory.js
@@ -9,6 +9,8 @@ export default settings => {
     /* istanbul ignore next */
     if (!cookiesEnabled()) return;
     const Store = createStore();
+
+    if (!settings.tid) console.warn('The tid setting is missing. A tid is required for banner measurements.');
     
     //extractFromCookie adds a try/catch guard for cookie reading and JSON.parse in case of cookie name collisions caused by versioning
     //for sites that are saving the cookie consent in a different shape, i.e. without cid and consent properties
