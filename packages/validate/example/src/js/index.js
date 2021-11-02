0dcf48db3168e7efa916c1e00f1e3714fe9c7cce
Mick Perkins <mick.perkins@stormid.com>
2021-11-02 17:51:55 +0000

    Update addMethod to accept array of fields

--- packages/validate/example/src/js/index.js
+++ packages/validate/example/src/js/index.js
@@ -1,6 +1,6 @@
 import validate from '../../../src';
 {
-    const validator = validate('form');
+    const [ validator ] = validate('form');
 
     // const later = document.getElementById('Later');
     // document.querySelector('.js-add').addEventListener('click', e => {
@@ -12,4 +12,18 @@ import validate from '../../../src';
     //         validator[0].addGroup([later]);
     //     }
     // });
+    const inputs = [ document.querySelector('#f1'), document.querySelector('#f2') ];
+    validator.addMethod(
+        'CustomGroup',
+        (value, fields) => {
+            console.log(value);
+            console.log(inputs);
+            return inputs[0].value.trim() === 'potato' || inputs[1].value.trim() === 'potato';
+        },
+        'One of the inputs must be the word "potato"',
+        inputs
+    );
+
+    console.log(validator.getState());
+
 };
