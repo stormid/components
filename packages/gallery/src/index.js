71f610e7e64d6c61fd58cdb94df75f791e28513e
Mick Perkins <mick.perkins@stormid.com>
2022-01-06 22:16:31 +0000

    Initial commit

---
+++ packages/gallery/src/index.js
@@ -0,0 +1,27 @@
+import factory from './lib/factory';
+
+export default (src, options) => {
+    if (!src.length) return void console.warn('Gallery cannot be initialised, no images found');
+
+    let items;
+
+    if (typeof src === 'string'){
+        const els = [].slice.call(document.querySelectorAll(src));
+
+        if (!els.length) return void console.warn('Gallery cannot be initialised, no images found');
+		
+        items = els.map(el => ({
+            trigger: el,
+            src: el.getAttribute('href'),
+            srcset: el.getAttribute('data-srcset') || null,
+            sizes: el.getAttribute('data-sizes') || null,
+            title: el.getAttribute('data-title') || '',
+            description: el.getAttribute('data-description') || ''
+        }));
+    } else items = src;
+
+    return Object.create(factory({
+        items,
+        settings: { ...defaults, ...options }
+    }));
+};
