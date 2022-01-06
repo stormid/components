1ac2d32f017a89c82fb225569819d37ee0203d0f
Mick Perkins <mick.perkins@stormid.com>
2022-01-06 23:18:16 +0000

    Updates dom and initialisation

--- packages/gallery/src/lib/constants.js
+++ packages/gallery/src/lib/constants.js
@@ -1,11 +1,1 @@
-/* istanbul ignore file */
-export const KEY_CODES = {
-    TAB: 9,
-    ESC: 27,
-    LEFT: 37,
-    RIGHT: 39
-};
-
 export const TRIGGER_EVENTS = ['click', 'keydown' ];
-
-export const FOCUSABLE_ELEMENTS = ['a[href]', 'area[href]', 'input:not([disabled])', 'select:not([disabled])', 'textarea:not([disabled])', 'button:not([disabled])', 'iframe', 'object', 'embed', '[contenteditable]', '[tabindex]:not([tabindex="-1"])'];
