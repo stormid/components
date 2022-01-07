48e2fc9d715697254c48454250d1f01860861cb4
Mick Perkins <mick.perkins@stormid.com>
2022-01-07 17:33:16 +0000

    Add basic functionality, add tests

--- packages/gallery/src/lib/constants.js
+++ packages/gallery/src/lib/constants.js
@@ -1,1 +1,8 @@
-export const TRIGGER_EVENTS = ['click', 'keydown' ];
+export const ATTRIBUTE = {
+    SOURCES: 'data-gallery-item-sources',
+    SRCSET: 'data-gallery-item-srcset',
+    SIZES: 'data-gallery-item-sizes',
+    SRC: 'data-gallery-item-src',
+    ALT: 'data-gallery-item-alt',
+    LOADED: 'data-gallery-item-loaded'
+};
