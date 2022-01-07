c2dd3683f869e8ae0496ff31c36d021e70b98824
Mick Perkins <mick.perkins@stormid.com>
2022-01-07 11:05:48 +0000

    Starts gallery mark up

--- packages/gallery/src/lib/defaults/index.js
+++ packages/gallery/src/lib/defaults/index.js
@@ -1,11 +1,13 @@
 /* istanbul ignore file */
-import * as templates from './templates';
 
 export default {
     preload: false,
     totals: true,
     start: 0,
-    fullscreen: true,
-    container: '.js-gallery__container',
-    templates
+    selectors: {
+        fullscreen: '[data-gallery-fullscreen]',
+        total: '[data-gallery-total]',
+        previous: '[data-gallery-previous]',
+        next: '[data-gallery-next]'
+    }
 };
