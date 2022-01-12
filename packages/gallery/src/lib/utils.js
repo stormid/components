d8ca9ff8b3ab9435f363f8586f8e723316c2a645
Mick Perkins <mick.perkins@stormid.com>
2022-01-12 22:03:15 +0000

    Add error hanlding tests

--- packages/gallery/src/lib/utils.js
+++ packages/gallery/src/lib/utils.js
@@ -3,22 +3,26 @@ import { ATTRIBUTE } from './constants';
 export const sanitise = item => item.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
 
 export const composeItems = (nodes, settings) => nodes.map(node => {
-    //must contain an img element
-    const imgContainer = node.querySelector(settings.selector.imgContainer);
-    const imgNode = imgContainer.querySelector('img');
-    if (!imgNode) return void console.warn('Gallery cannot find image');
-
-    const item = {
+    let item = {
         node,
         sources: node.getAttribute(ATTRIBUTE.SOURCES),
         srcset: node.getAttribute(ATTRIBUTE.SRCSET),
         sizes: node.getAttribute(ATTRIBUTE.SIZES),
         src: node.getAttribute(ATTRIBUTE.SRC),
         alt: node.getAttribute(ATTRIBUTE.ALT),
-        imgContainer,
-        loaded: node.hasAttribute(ATTRIBUTE.LOADED) || imgNode.getAttribute('src') === node.getAttribute(ATTRIBUTE.SRC)
     };
-    if (!item.sources && !item.srcset && !item.src && !item.loaded) return void console.warn('Gallery cannot be initialised, no image sources found');
+    //must contain an img container element
+    const imgContainer = node.querySelector(settings.selector.imgContainer);
+    if (!imgContainer) return console.warn(`Gallery cannot find image container ${settings.selector.imgContainer}`), item;
+    const imgNode = imgContainer.querySelector('img');
+
+    item = {
+        ...item,
+        imgContainer,
+        loaded: node.hasAttribute(ATTRIBUTE.LOADED) || (imgNode && imgNode.getAttribute('src') === node.getAttribute(ATTRIBUTE.SRC))
+    };
+    if (!item.sources && !item.srcset && !item.src && !item.loaded) return void console.warn('Gallery cannot be initialised, no image sources found'), item;
+    
     return item;
 });
 
