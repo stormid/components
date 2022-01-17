5ea34bb9ef25632a42a9e3ebf2f3fd3fe6414110
Mick Perkins <mick.perkins@stormid.com>
2022-01-17 17:37:04 +0000

    Add URL updating, retrieving index from URL, popstate handler

--- packages/gallery/src/index.js
+++ packages/gallery/src/index.js
@@ -5,5 +5,5 @@ export default (selector, options) => {
     const galleries = [].slice.call(document.querySelectorAll(selector));
     if (galleries.length === 0) return void console.warn('Gallery cannot be initialised, no galleries found');
     
-    return galleries.map(gallery => Object.create(factory(gallery, { ...defaults, ...options })));
+    return galleries.map((gallery, index) => Object.create(factory(gallery, { ...defaults, ...options }, index)));
 };
