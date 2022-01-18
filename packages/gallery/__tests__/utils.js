675b264e4dcd9a46bcbfd81c4d640550eba8a64a
Mick Perkins <mick.perkins@stormid.com>
2022-01-18 13:02:53 +0000

    Refactor and add utils tests

--- packages/gallery/__tests__/utils.js
+++ packages/gallery/__tests__/utils.js
@@ -1,4 +1,4 @@
-import { sanitise } from '../src/lib/utils';
+import { sanitise, getIndexFromURL } from '../src/lib/utils';
 
 describe('Gallery > Utils > sanitize', () => {
 
@@ -23,3 +23,58 @@ describe('Gallery > Utils > sanitize', () => {
 //composeItems
 
 //composeDOM
+
+//getIndexFromURL
+// export const getIndexFromURL = (name, items, url, fallback = false) => {
+//     try {
+//         const hash = url.split(`#`)[1];
+//         if (hash.indexOf(`${name}-`) === -1) return fallback;
+//         const num = Number(hash.split(`${name}-`)[1]);
+//         if (isNaN(num)) throw 'Gallery hash not valid';
+//         const index = num - 1;
+//         if (index < 0 || index > (items.length - 1)) throw 'Gallery index out of bounds';
+//         return index;
+//     } catch (e){
+//         return fallback;
+//     }
+// };
+describe('Gallery > getIndexFromURL', () => {
+    //return fallback if url.split throws
+    it('Should return fallback if url.split throws', () => {
+        const warn = jest.spyOn(console, 'warn').mockImplementation(() => {});
+        const result = getIndexFromURL('test-name', [], '#');
+        expect(result).toEqual(false);
+        warn.mockRestore();
+    });
+    
+    it('Should return fallback if hash does not contain gallery name', () => {
+        const warn = jest.spyOn(console, 'warn').mockImplementation(() => {});
+        const result = getIndexFromURL('test-name', [], '#potato');
+        expect(result).toEqual(false);
+        warn.mockRestore();
+    });
+    
+    it('Should return fallback and console warn if hash does not contain a number', () => {
+        const warn = jest.spyOn(console, 'warn').mockImplementation(() => {});
+        const result = getIndexFromURL('test-name', [], '#test-name-potato');
+        expect(result).toEqual(false);
+        expect(warn).toHaveBeenCalledWith('Gallery hash not valid');
+        warn.mockRestore();
+    });
+
+    it('Should return fallback and console warn if index is out of bounds', () => {
+        const warn = jest.spyOn(console, 'warn').mockImplementation(() => {});
+        const result = getIndexFromURL('test-name', [{}, {}], '#test-name-4');
+        expect(result).toEqual(false);
+        expect(warn).toHaveBeenCalledWith('Gallery index out of bounds');
+        warn.mockRestore();
+    });
+
+    it('Should return index if one is found', () => {
+        const result = getIndexFromURL('test-name', [{}, {}], '#test-name-1');
+        //index is one less than the number in the hash
+        expect(result).toEqual(0);
+    });
+
+
+});
