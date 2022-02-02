9032c99373cb021483a94843419c7dc116efb6ce
Sarah Richards <sarah.richards@stormid.com>
2022-02-02 16:17:06 +0000

    modal tests

--- packages/modal/__tests__/unit/init.js
+++ packages/modal/__tests__/unit/init.js
@@ -1,4 +1,5 @@
 import modal from '../../src';
+import { getSelection } from '../../src';
 
 let ModalSet;
 
@@ -122,6 +123,56 @@ describe(`Modal > Initialisation`, () => {
         expect(toggles.length).toEqual(2);
         expect(toggles[0].getAttribute('data-id')).toEqual('toggle-1');
         expect(toggles[1].getAttribute('data-id')).toEqual('toggle-2');
+    });
+
+});
+
+describe('Modal > Initialisation > Get Selection', () => {
+
+    const setupDOM = () => {
+        document.body.innerHTML = `<div id="modal-1" class="js-modal modal" data-modal-toggle="js-modal-toggle">
+            <div class="modal__inner" role="dialog" aria-modal="true" aria-labelledby="modal-label">
+                <h1 id="modal-label">Modal</h1>
+                <button>Focusable element</button>
+                <input type="text">
+                <input type="text">
+                <button aria-label="close" class="modal__close-btn js-modal-toggle" data-id="toggle-2">
+                    <svg fill="#fff" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/>
+                        <path d="M0 0h24v24H0z" fill="none"/>
+                    </svg>
+                </button>
+            </div>
+        </div>`;
+    }
+
+    beforeAll(setupDOM);
+
+    it('should return an array when passed a DOM element', async () => {
+        const modal = document.querySelector('.js-modal');
+        const els = getSelection(modal);
+        expect(els instanceof Array).toBe(true);
+        expect(els.length).toEqual(1);
+    });
+
+    it('should return an array when passed a NodeList element', async () => {
+        const modal = document.querySelectorAll('.js-modal');
+        const els = getSelection(modal);
+        expect(els instanceof Array).toBe(true);
+        expect(els.length).toEqual(1);
+    });
+
+    it('should return an array when passed an array of DOM elements', async () => {
+        const modal = document.querySelector('.js-modal');
+        const els = getSelection([modal]);
+        expect(els instanceof Array).toBe(true);
+        expect(els.length).toEqual(1);
+    });
+
+    it('should return an array when passed a string', async () => {
+        const els = getSelection('.js-modal');
+        expect(els instanceof Array).toBe(true);
+        expect(els.length).toEqual(1);
     });
 
 });
