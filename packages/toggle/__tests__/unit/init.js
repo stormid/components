b2439a6e5aff2f4886a1ccadf7df3313c399c7a9
Sarah Richards <sarah.richards@stormid.com>
2022-02-02 15:18:05 +0000

    Toggle unit test additions

--- packages/toggle/__tests__/unit/init.js
+++ packages/toggle/__tests__/unit/init.js
@@ -1,4 +1,5 @@
 import toggle from '../../src';
+import { getSelection } from '../../src';
 
 let Toggles, ToggleLocals;
 
@@ -47,6 +48,50 @@ describe(`Toggle > Init`, () => {
     it('should use data attributes as settings, overriding options', () => {
         expect(ToggleLocals[0].getState().settings.startOpen).toEqual('true');
         expect(ToggleLocals[0].getState().isOpen).toEqual(true);
+    });
+
+});
+
+describe('Toggle > Initialisation > Get Selection', () => {
+
+    // Set up our document body
+    document.body.innerHTML = `<a tabindex="0" id="btn-1-1" href="#target-1" class="js-toggle_btn">Test toggle</a>
+        <a href="#target-1" class="js-toggle_btn">Test toggle</a>
+        <div id="target-1" class="js-toggle" data-toggle="js-toggle_btn">
+            <div id="focusable-1-1" tabindex="0">Test focusable content</div>
+            <div tabindex="0">Test focusable content</div>
+            <div tabindex="0">Test focusable content</div>
+        </div>
+        
+        <button id="target-4" class="js-toggle__btn-2">Test toggle</button>
+        <div id="target-4" class="js-toggle-local" data-toggle="js-toggle__btn-2" data-start-open="true"></div>`;
+
+
+    it('should return an array when passed a DOM element', async () => {
+        const toggles = document.querySelector('.js-toggle');
+        const els = getSelection(toggles);
+        expect(els instanceof Array).toBe(true);
+        expect(els.length).toEqual(1);
+    });
+
+    it('should return an array when passed a NodeList element', async () => {
+        const toggles = document.querySelectorAll('.js-toggle');
+        const els = getSelection(toggles);
+        expect(els instanceof Array).toBe(true);
+        expect(els.length).toEqual(1);
+    });
+
+    it('should return an array when passed an array of DOM elements', async () => {
+        const toggles = document.querySelector('.js-toggle');
+        const els = getSelection([toggles]);
+        expect(els instanceof Array).toBe(true);
+        expect(els.length).toEqual(1);
+    });
+
+    it('should return an array when passed a string', async () => {
+        const els = getSelection('.js-toggle');
+        expect(els instanceof Array).toBe(true);
+        expect(els.length).toEqual(1);
     });
 
 });
