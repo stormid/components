7e568f243cbe20b6e8f359df795d54621e1e4bfd
Sarah Richards <62299569+sarah-storm@users.noreply.github.com>
2022-06-01 09:44:03 +0100

    Adding validateGroup api option (#213)

--- packages/validate/src/lib/factory/group.js
+++ packages/validate/src/lib/factory/group.js
@@ -1,6 +1,11 @@
-import { removeUnvalidatableGroups, assembleValidationGroup } from '../validator';
+import { 
+    removeUnvalidatableGroups, 
+    assembleValidationGroup, 
+    getGroupValidityState, 
+    reduceGroupValidityState,
+    reduceErrorMessages } from '../validator';
 import { initRealTimeValidation } from '../validator/real-time-validation';
-import { clearError, addAXAttributes } from '../dom';
+import { renderError, clearError, addAXAttributes } from '../dom';
 import { ACTIONS } from '../constants';
 
 /**
@@ -23,7 +28,37 @@ export const addGroup = Store => nodes => {
 };
 
 /**
- * Removes a group to the validation model, used via the API
+ * Validates a group individually, used via the API
+ * 
+ * @param groupName, nodes comprising the group to be validated
+ * 
+ * @returns [Promise] Resolves with boolean validityState of the group
+ */
+export const validateGroup = Store => groupName => {
+    return new Promise(resolve => {
+        if(!Store.getState().groups[groupName].valid && Store.getState().errors[groupName]) {
+            Store.dispatch(ACTIONS.CLEAR_ERROR, groupName, [clearError(groupName)]);
+        }
+        getGroupValidityState(Store.getState().groups[groupName])
+        .then(res => {
+            if(!res.reduce(reduceGroupValidityState, true)) {
+                Store.dispatch(
+                    ACTIONS.VALIDATION_ERROR,
+                    {
+                        group: groupName,
+                        errorMessages: res.reduce(reduceErrorMessages(groupName, Store.getState()), [])
+                    },
+                    [renderError(groupName)]
+                );
+                return resolve(false);
+            }
+            return resolve(true);
+        });
+    })
+};
+
+/**
+ * Removes a group from the validation model, used via the API
  * Dispatches remove group method to store 
  * 
  * @param groupName, nodes comprising the group
