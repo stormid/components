ad35c324d9c055dd6c3ad9c8bc0fcb43b63ee2b2
Mick Perkins <mick.perkins@stormid.com>
2023-04-06 16:28:08 +0100

    Update tab left/right behvaiour to match APG

--- packages/tabs/src/lib/factory.js
+++ packages/tabs/src/lib/factory.js
@@ -16,8 +16,7 @@ export default ({ node, settings }) => {
         settings,
         node,
         activeIndex: activeIndex !== undefined ? +activeIndex : +settings.activeIndex,
-        //activeTabIndex should initially match the active panel, so initialising here to the same value
-        activeTabIndex: activeIndex !== undefined ? +activeIndex : +settings.activeIndex, 
+        activeTabIndex: activeIndex !== undefined ? +activeIndex : +settings.activeIndex,
         tabs,
         panels,
         loaded: false
