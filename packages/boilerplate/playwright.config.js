bc21c55799f6c0f1645c8d99bdb4334e22c2a2f6
Sarah Richards <sarah.richards@stormid.com>
2025-03-05 11:55:47 +0000

    Update boilerplate

--- packages/boilerplate/playwright.config.js
+++ packages/boilerplate/playwright.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require('@playwright/test');
-const base = require('../../tools/playwright/config.base.js');
+const baseConfig = require('../../tools/playwright/config.base.js');
 
 module.exports = defineConfig({
-    ...base
+    ...baseConfig
 })
