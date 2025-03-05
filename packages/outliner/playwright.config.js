9c00a72b3e7d48f5983440d0b6bed2a464210b07
Sarah Richards <sarah.richards@stormid.com>
2025-03-05 10:57:58 +0000

    Update port number for parallel tests in github

--- packages/outliner/playwright.config.js
+++ packages/outliner/playwright.config.js
@@ -1,5 +1,11 @@
 const base = require('../../tools/playwright/config.base.js');
 
 module.exports = {
-    ...base
+    ...base,
+    use: {
+        baseURL: 'http://127.0.0.1:8082',
+    },
+    webServer: {
+        url: 'http://127.0.0.1:8082',
+    },
 }
