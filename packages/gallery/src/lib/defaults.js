7bb5f0807f3dfbda6e192b0038e5b52e2ba6e003
Mick <mick.perkins@stormid.com>
2025-03-13 18:34:36 +0000

    Update implementation to remove loaders

--- packages/gallery/src/lib/defaults.js
+++ packages/gallery/src/lib/defaults.js
@@ -1,7 +1,7 @@
 /* istanbul ignore file */
 
 export default {
-    name: false, //for setting the URL, of not set (false) it will get it from the id the container node, or just 'gallery'
+    name: false, //for setting the URL, if not set (false) it will get it from the id the container node, or just 'gallery'
     preload: false,
     totals: true,
     startIndex: 0,
@@ -11,16 +11,13 @@ export default {
         liveRegion: '[data-gallery-live-region]',
         previous: '[data-gallery-previous]',
         next: '[data-gallery-next]',
-        imgContainer: '[data-gallery-item-img-container]',
-        loader: '[data-gallery-item-loader]',
         navigate: '[data-gallery-navigate]'
     },
     className: {
         active: 'is--active',
-        loading: 'is--loading',
         fullscreen: 'is--fullscreen'
     },
-    manualInitialisation: false, //if the gallery is hidden (e.g. in a modal) we may wish to delay initialisation and image loading until it is activiated
+    manualInitialisation: false, //if the gallery is hidden (e.g. in a modals) we may wish to delay initialisation
     updateURL: true, //change URL when item changes
     announcement(current, total){
         return `${current} of ${total}`;
