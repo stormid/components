3c355d9ae38d31a9d961286aa44a519823f528c3
Sarah Richards <sarah.richards@stormid.com>
2025-03-31 16:04:59 +0100

    Undo reporter

--- tools/playwright/config.base.js
+++ tools/playwright/config.base.js
@@ -6,10 +6,7 @@ module.exports = {
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-  reporter: [
-    ['line'],
-    ['html', { open: 'never' }]
-  ],
+  reporter: 'line',
   timeout: 60_000,
   expect: {
     timeout: 10_000,
