031c3f5c509a2115c071ecaae1dcf11ab0c98419
Sarah Richards <sarah.richards@stormid.com>
2025-08-25 16:33:55 +0100

    pre-submit tests

--- packages/validate/__tests__/playwright/playwright.spec.js
+++ packages/validate/__tests__/playwright/playwright.spec.js
@@ -11,10 +11,65 @@ test.beforeEach(async ({ page }, testInfo) => {
 });
 
 
-test.describe('Outliner > Axe', { tag: '@reduced'}, () => {
+test.describe('Validate > Axe', { tag: '@reduced'}, () => {
 	test('Should not have any automatically detectable accessibility issues', async ({ page }) => {	
 		const accessibilityScanResults = await new AxeBuilder({ page }).analyze(); 
 		expect(accessibilityScanResults.violations).toEqual([]);
 	});
+});
+
+
+test.describe('Validate > Post-validation', { tag: '@all'}, () => {
+
+	test('Should add correct user input to the form data if the form is valid', async ({ page }) => {	
+
+		await page.route('**/test', async (route, request) => {
+			const postData = request.postData();
+			const params = new URLSearchParams(postData);
+			expect(params.get('fname')).toBe('John');
+			expect(params.get('lname')).toBe('Doe');
+			await route.fulfill({ status: 200, body: 'OK' });
+		});
+
+		await page.goto('/mini.html');
+		await page.fill("#fname", "John");
+		await page.fill("#lname", "Doe");
+		await page.click("#submit");
+
+	});
+
+	test('Should mutate the action attribute of the form if the submit button clicked has a formaction', async ({ page }) => {
+
+		await page.route('**/alternative', async (route, request) => {
+			const postData = request.postData();
+			const params = new URLSearchParams(postData);
+			expect(params.get('fname')).toBe('John');
+			expect(params.get('lname')).toBe('Doe');
+			await route.fulfill({ status: 200, body: 'OK' });
+		});
+
+		await page.goto('/mini.html');
+		await page.locator('#submit').evaluate((button) => {
+			button.setAttribute('formaction', '/alternative');
+		});
+		await page.fill("#fname", "John");
+		await page.fill("#lname", "Doe");
+		await page.click("#submit");
+	});
+
+	test('Should call the presubmit hook pre-submit', async ({ page }) => {
+		await page.route('**/test', async (route, request) => {
+			const postData = request.postData();
+			const params = new URLSearchParams(postData);
+			expect(params.get('hiddenCheck')).toBe('true');
+			await route.fulfill({ status: 200, body: 'OK' });
+		});
+
+		await page.goto('/mini.html');
+		await page.fill("#fname", "John");
+		await page.fill("#lname", "Doe");
+		await page.click("#submit");
+	});
+	
 });
 
