d9045721b99702667889334fc4208c2c7cd74047
Catriona Vine <catriona.vine@stormid.com>
2026-07-21 10:53:10 +0100

    List all autocomplete options in defaults with null placeholders

--- packages/autocomplete/src/lib/defaults.js
+++ packages/autocomplete/src/lib/defaults.js
@@ -1,26 +1,32 @@
 /* node:coverage disable */
 
 export default {
+    name: null,
+    search: null,
+    values: [],
+    list: false,
     minlength: 2,
     maxResults: 6,
     multiple: false,
     async: false,
-    noResultsMsg: 'No results found',
-    loadingMsg: 'Loading…',
-    hintMsg(minlength) {
-        return `Type ${minlength} or more characters for results`;
-    },
-    inputClassName: 'autocomplete__input',
-    placeholder: '',
-    confirmOnBlur: true,
-    allowFreeText: false,
-    list: false,
-    values: [],
+    value: null,
+    label: null,
     displayTemplate(option) {
         return option.value;
     },
     optionTemplate: false,
     submissionTemplate(option) {
         return option.value;
+    },
+    allowFreeText: false,
+    confirmOnBlur: true,
+    clearOnBlur: false,
+    placeholder: '',
+    inputClassName: 'autocomplete__input',
+    id: null,
+    noResultsMsg: 'No results found',
+    loadingMsg: 'Loading…',
+    hintMsg(minlength) {
+        return `Type ${minlength} or more characters for results`;
     }
 };
